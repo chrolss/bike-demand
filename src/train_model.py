1b8acc2723ad12321eb2842a00d0da03711d9c71
chrolss <chrolss@kth.se@>
2020-05-28 21:06:07 +0200

    first basic lgb model with 1 shift

--- src/train_model.py
+++ src/train_model.py
@@ -10,3 +10,29 @@ test = joblib.load('data/processed/test.pkl')
 # The model needs to be able to create a prognosis for max 12 days with 24h resolution.
 # This equates to 288 hours ahead in the future, based on the latest measurement
 
+train['k1'] = train['count'].shift(1)
+train.dropna(inplace=True)
+
+lgb_params = {
+    'objective': 'regression',
+    'boosting': 'gbdt',
+    'verbose': 1,
+    'metric': 'rmse'
+}
+
+X_train, X_test, y_train, y_test = train_test_split(train.drop(['count'], axis='columns'), train['count'], test_size=.3)
+
+lgb_estimator = lgb.train(
+    lgb_params,
+    train_set=lgb.Dataset(X_train, y_train),
+    valid_sets=lgb.Dataset(X_test, y_test),
+    num_boost_round=500
+)
+
+preds = lgb_estimator.predict(X_test)
+
+# Doing some evaluation
+import matplotlib.pyplot as plt
+_ = plt.plot(y_test.values, 'bo')
+_ = plt.plot(preds, 'r-')
+plt.show()
